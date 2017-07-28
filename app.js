d2a8799e2378f9dac3a060b48c97e04ae7f569e8
Jase Cutler <jasecutler@gmail.com>
2017-07-28 14:49:26 -0700

    Added game play routes and some conditional logic

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const mustacheExpress = require('mustache-express');
 const executePlay = require('./app/hangman.js')
 const session = require('express-session')
+const bodyParser = require('body-parser')
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
@@ -16,16 +17,38 @@ app.use(session({
 }))
 
 app.use(express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", function(req, res, next){
-  if(req.session){
-    req.session.guess = "a"
-    req.session = executePlay(req.session)
-    console.log(req.session)
-  }
   res.render("index", {appType:"Express"})
 })
 
+app.post("/start", function(req,res, next){
+  req.session.currentGame = false;
+  req.session = executePlay(req.session)
+  res.redirect("/game")
+})
+app.get("/game", function(req,res,next){
+  if(req.session.currentGame){
+    res.render("gamePlay",req.session)
+  } else if(req.session.wordSolved){
+    res.redirect("winner")
+  } else {
+    res.redirect("loser")
+  }
+})
+app.post("/taketurn",function(req,res,next){
+  req.session.guess = req.body.guess
+  req.session = executePlay(req.session)
+  res.redirect("/game")
+})
+app.get("/winner",function(req,res,next){
+  res.render("winnersPage",req.session)
+})
+app.get("/loser", function(req,res,next){
+  res.render("losersPage",req.session)
+})
 app.listen(3000, function(){
   console.log("App running on port 3000")
 })
