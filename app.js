69d39275f33ff74c0e839a5109223a9b9272ca46
Jase Cutler <jasecutler@gmail.com>
2017-07-28 11:02:03 -0700

    Setup session usage

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mustacheExpress = require('mustache-express');
-const gameSession = require('./function_tests/refactor.js')
+const gameSession = require('./app/hangman.js')
+const session = require('express-session')
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
 gameSession.gamePlay()
 app.use(express.static(path.join(__dirname, 'static')))
 
