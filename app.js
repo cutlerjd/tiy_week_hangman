727d67a19508e518db5432ff8efc69355c1ee8c6
Jase Cutler <jasecutler@gmail.com>
2017-07-31 15:55:20 -0700

    Updated to use a new game route

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const mustacheExpress = require('mustache-express');
+const mustacheExpress = require('mustache-express')
 const executePlay = require('./model/hangman.js')
 const session = require('express-session')
 const bodyParser = require('body-parser')
+const gamePlayRouter = require('./routes/gamePlayRouter')
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
@@ -20,26 +21,10 @@ app.use(express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use('/game',gamePlayRouter)
+
 app.get("/", function(req, res, next){
   res.render("index")
-})
-
-app.post("/start", function(req,res, next){
-  req.session.currentGame = false;
-  req.session = executePlay(req.session)
-  res.redirect("/game")
-})
-app.get("/game", function(req,res,next){
-  if(req.session.currentGame){
-    res.render("gamePlay",req.session)
-  } else {
-    res.render("results",req.session)
-  }
-})
-app.post("/taketurn",function(req,res,next){
-  req.session.guess = req.body.guess
-  req.session = executePlay(req.session)
-  res.redirect("/game")
 })
 
 app.listen(3000, function(){
---
+++ routes/gamePlayRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express.Router();
+const executePlay = require('../model/hangman.js')
+
+
+app.post("/start", function(req,res, next){
+  req.session.currentGame = false;
+  req.session = executePlay(req.session)
+  res.redirect("/game")
+})
+
+app.get("/game", function(req,res,next){
+  if(req.session.currentGame){
+    res.render("gamePlay",req.session)
+  } else {
+    res.render("results",req.session)
+  }
+})
+
+app.post("/taketurn",function(req,res,next){
+  req.session.guess = req.body.guess
+  req.session = executePlay(req.session)
+  res.redirect("/game")
+})
+
+module.exports = app
